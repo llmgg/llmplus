c2e51122507d644a8f7c0b6e0542e68a0069bcec
llmgg <xiongwen.wxw@foxmail.com>
2023-09-01 08:45:15 +0800

    update voc_embedding

--- llmplus/layer/voc_embedding.py
+++ llmplus/layer/voc_embedding.py
@@ -4,9 +4,13 @@ import torch.nn as nn
 
 
 class VocEmbedding(nn.Module):
-    def __init__(self, voc_size: int, dim: int):
+    def __init__(self, voc_size: int, dim: int, padding_idx = None):
         super(VocEmbedding, self).__init__()
-        self.embedding = nn.Embedding(voc_size, dim)
+        self.embedding = nn.Embedding(
+            num_embeddings=voc_size,
+            embedding_dim=dim,
+            padding_idx=padding_idx,
+        )
         self.coe = math.sqrt(dim)
 
     def forward(self, x: torch.Tensor):
